a7f85b354cf70bb72f3b588e79b33ff9333119d9
KlakCodes <harrisonjclark@hotmail.com>
2025-01-03 14:04:57 +0000

    Created folder structure and basic home skeleton
    Created new folders for assets, styles and scripts
    
    Began creating the script to create home html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 import "./styles.css";
 
 console.log('Hello world!');
+
+const content = document.querySelector('#content');
+
+
+// Home - Header
+const header = document.createElement('div');
+header.classList.toggle('home-header');
+const headerTitle = document.createElement('h1')
+headerTitle.textContent = 'Norse Roast';
+header.appendChild(headerTitle);
+
+content.appendChild(header);
+
+// Home - About
+const about = document.createElement('div');
+about.classList.toggle('home-about');
+about.textContent = 'Welcome to Norse Roast - Home to the best coffee in the nine realms!';
+
+content.appendChild(about);
+
+// Home - Opening Hours
+const openingHours = document.createElement('div');
+openingHours.classList.toggle('home-hours');
+const hoursTitle = document.createElement('h2');
+hoursTitle.textContent = 'Opening Hours';
+openingHours.appendChild(hoursTitle);
+
+const hoursList = document.createElement('ul');
+const mon = document.createElement('li');
+mon.textContent = 'Monday: 8:00am - 8:00pm';
+hoursList.appendChild(mon);
+
+const tue = document.createElement('li');
+tue.textContent = 'Tuesday: 8:00am - 8:00pm';
+hoursList.appendChild(tue);
+
+const wed = document.createElement('li');
+wed.textContent = 'Wednesday: 8:00am - 8:00pm';
+hoursList.appendChild(wed);
+
+const thu = document.createElement('li');
+thu.textContent = 'Thursday: 8:00am - 8:00pm';
+hoursList.appendChild(thu);
+
+const fri = document.createElement('li');
+fri.textContent = 'Friday: 8:00am - 8:00pm';
+hoursList.appendChild(fri);
+
+const sat = document.createElement('li');
+sat.textContent = 'Saturday: 8:00am - 10:00pm';
+hoursList.appendChild(sat);
+
+const sun = document.createElement('li');
+sun.textContent = 'Sunday: 10:00am - 4:30pm';
+hoursList.appendChild(sun);
+
+openingHours.appendChild(hoursList);
+
+content.appendChild(openingHours);
+
+// Home - Location
+const location = document.createElement('div');
+location.classList.toggle('home-location');
+const locationTitle = document.createElement('h2');
+locationTitle.textContent = 'Location';
+location.appendChild(locationTitle);
+const locationAddress = document.createElement('p');
+locationAddress.textContent = 'Address 1, Address 2, Adress 3, Address 4';
+location.appendChild(locationAddress);
+
+content.appendChild(location);

7476ffad0be17f571786f4ca9faf67a4164721e0
KlakCodes <harrisonjclark@hotmail.com>
2025-01-06 15:38:08 +0000

    Added script to create elements for menu/contact

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -3,9 +3,36 @@ import '../styles/contact.css';
 function contactPage() {
     const content = document.querySelector('#content');
 
-    const testContact = document.createElement('h1');
-    testContact.textContent = 'Contact Information';
-    content.appendChild(testContact);
+    // Contact - Header
+    const header = document.createElement('div');
+    header.classList.toggle('contact-header');
+    const headerTitle = document.createElement('h1')
+    headerTitle.textContent = 'Contact Information';
+    header.appendChild(headerTitle);
+
+    content.appendChild(header);
+
+    // Contact - Telephone
+    const telHeader = document.createElement('h2');
+    telHeader.classList.toggle('contact-subHeader');
+    telHeader.textContent = 'Telephone'
+    
+    const telephone = document.createElement('p');
+    telephone.textContent = 'Coming soon!';
+
+    content.appendChild(telHeader);
+    content.appendChild(telephone);
+
+    // Contact - Email
+    const emailHeader = document.createElement('h2');
+    emailHeader.classList.toggle('contact-subHeader;');
+    emailHeader.textContent = 'Email';
+
+    const email = document.createElement('p');
+    email.textContent = 'norse.roast@norsemail.com';
+
+    content.appendChild(emailHeader);
+    content.appendChild(email);
 }
 
 export default contactPage;
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,9 +3,29 @@ import '../styles/menu.css';
 function menuPage() {
     const content = document.querySelector('#content');
 
-    const testMenu = document.createElement('h1');
-    testMenu.textContent = 'Menu';
-    content.appendChild(testMenu);
+    // Menu - Header
+    const header = document.createElement('div');
+    header.classList.toggle('menu-header');
+    const headerTitle = document.createElement('h1')
+    headerTitle.textContent = 'Menu';
+    header.appendChild(headerTitle);
+
+    content.appendChild(header);
+
+    // Menu - Items
+    const menuContainer = document.createElement('div');
+    menuContainer.classList.toggle('menu-container');
+
+    content.appendChild(menuContainer);
+
+    const menuItems = ["Latte","Americano","Cappuccino","Macchiato","Flat White","Espresso", "Mocha", "Hot Chocolate"];
+    menuItems.forEach(item => {
+        const itemCard = document.createElement('div');
+        itemCard.classList.toggle('menu-item');
+        itemCard.textContent = item;
+        
+        menuContainer.appendChild(itemCard);
+    })
 }
 
 export default menuPage;
